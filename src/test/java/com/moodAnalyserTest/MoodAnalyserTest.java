2ca9ecf7e18a1ad2a93ef1a912548cec88d25c54
bolishettianjali <bolishettianjali@gmail.com>
2021-11-29 22:24:48 +0530

    Added: Constructor to take message as parameters and assign message field

--- src/test/java/com/moodAnalyserTest/MoodAnalyserTest.java
+++ src/test/java/com/moodAnalyserTest/MoodAnalyserTest.java
@@ -7,22 +7,24 @@ import org.junit.jupiter.api.Test;
 
 public class MoodAnalyserTest {
 
-    MoodAnalyser mood;
-
-    @BeforeEach
-    public void objectMethod() {
-        mood = new MoodAnalyser();
-    }
-
     @Test
     public void testAnalyseMoodForSad() {
+        MoodAnalyser mood = new MoodAnalyser();
         String result = mood.analyseMood("I am in sad mood");
         Assertions.assertEquals("SAD", result);
     }
 
     @Test
     public void testAnalyseMoodForAnyMood() {
+        MoodAnalyser mood = new MoodAnalyser();
         String result = mood.analyseMood("I am in any mood");
         Assertions.assertEquals("HAPPY", result);
+    }
+
+    @Test
+    public void testAnalyseMoodForSadUsingParamConst() {
+        MoodAnalyser mood = new MoodAnalyser("I am in sad mood");
+        String result = mood.analyseMood();
+        Assertions.assertEquals("SAD", result);
     }
 }
